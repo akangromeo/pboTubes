24773eb091c9708e0ac4a824d44ebb49202ffed3
akangromeo <deanagnia7@gmail.com>
2022-02-13 16:11:08 +0800

    project apparel
    check1

--- Project Apparel/src/project/apparel/ProjectApparel.java
+++ Project Apparel/src/project/apparel/ProjectApparel.java
@@ -15,6 +15,7 @@ public class ProjectApparel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("project apparel check");
     }
     
 }
